f0b6f76a03e100977ce394acc842b4331a570bef
Ben Blank <ben.blank@gmail.com>
2014-09-13 21:15:58 -0700

    fill out Result interface

--- src/main/java/com/five35/dex/Result.java
+++ src/main/java/com/five35/dex/Result.java
@@ -1,5 +1,7 @@
 package com.five35.dex;
 
+import java.util.List;
+import javax.annotation.Nonnull;
 import javax.annotation.concurrent.Immutable;
 
 /**
@@ -7,4 +9,15 @@ import javax.annotation.concurrent.Immutable;
  */
 @Immutable
 public interface Result {
+	/**
+	 * @return The result as a list.
+	 */
+	@Nonnull
+	List<Float> asList();
+
+	/**
+	 * @return The result as a floating-point number.
+	 */
+	@Nonnull
+	float asFloat();
 }
