381ab8099a60789b879e39be453e8a5c0ebf1ff1
Ben Blank <ben.blank@gmail.com>
2014-09-13 22:45:20 -0700

    add basic arithmetic operators

--- src/main/java/com/five35/dex/Symbol.java
+++ src/main/java/com/five35/dex/Symbol.java
@@ -13,6 +13,35 @@ import javax.annotation.concurrent.Immutable;
 public abstract class Symbol {
 	private static final Map<String, Symbol> SYMBOL_TABLE = new HashMap<>();
 
+	static final Symbol OPERATOR_ADD = new InfixSymbol("OPERATOR_ADD", "+", 10) {
+		@Override
+		Result binary(final Result left, final Result right) {
+			return new ScalarResult(left.asFloat() + left.asFloat());
+		}
+	};
+
+	// TODO: needs to work as a unary operator, too
+	static final Symbol OPERATOR_SUBTRACT = new InfixSymbol("OPERATOR_SUBTRACT", "-", 10) {
+		@Override
+		Result binary(final Result left, final Result right) {
+			return new ScalarResult(left.asFloat() - left.asFloat());
+		}
+	};
+
+	static final Symbol OPERATOR_MULTIPLY = new InfixSymbol("OPERATOR_MULTIPLY", "*", 20) {
+		@Override
+		Result binary(final Result left, final Result right) {
+			return new ScalarResult(left.asFloat() * left.asFloat());
+		}
+	};
+
+	static final Symbol OPERATOR_DIVIDE = new InfixSymbol("OPERATOR_DIVIDE", "/", 20) {
+		@Override
+		Result binary(final Result left, final Result right) {
+			return new ScalarResult(left.asFloat() / left.asFloat());
+		}
+	};
+
 	private final int leftBindingPower;
 	private final String name;
 
