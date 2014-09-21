601a5432e4b12df452a068afd266b1c9dfe509fd
Ben Blank <ben.blank@gmail.com>
2014-09-21 12:11:45 -0700

    null-check ALL THE THINGS (BinaryExpression)

--- src/main/java/com/five35/dex/BinaryExpression.java
+++ src/main/java/com/five35/dex/BinaryExpression.java
@@ -1,6 +1,7 @@
 package com.five35.dex;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 import java.util.Map;
 
 class BinaryExpression implements Expression {
@@ -9,13 +10,15 @@ class BinaryExpression implements Expression {
 	private final Expression right;
 
 	BinaryExpression(final InfixSymbol infix, final Expression left, final Expression right) {
-		this.infix = infix;
-		this.left = left;
-		this.right = right;
+		this.infix = Preconditions.checkNotNull(infix);
+		this.left = Preconditions.checkNotNull(left);
+		this.right = Preconditions.checkNotNull(right);
 	}
 
 	@Override
 	public Result execute(final Optional<Map<String, Expression>> variables) {
+		Preconditions.checkNotNull(variables);
+
 		return this.infix.binary(this.left.execute(variables), this.right.execute(variables));
 	}
 }
--- src/test/java/com/five35/dex/BinaryExpressionTest.java
+++ src/test/java/com/five35/dex/BinaryExpressionTest.java
@@ -1,15 +1,28 @@
 package com.five35.dex;
 
 import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
 import java.util.Map;
 import mockit.Expectations;
 import mockit.Injectable;
 import mockit.Mocked;
 import org.junit.Test;
 
-
-@SuppressWarnings({ "javadoc", "unused" })
+@SuppressWarnings({ "javadoc", "static-method", "unused" })
 public class BinaryExpressionTest {
+	@Test
+	public void ctor_checksForNullArguments(@Injectable final InfixSymbolTest.DummySymbol symbol, @Mocked final Expression left, @Mocked final Expression right) {
+		new Expectations(Preconditions.class) {
+			{
+				Preconditions.checkNotNull(symbol);
+				Preconditions.checkNotNull(left);
+				Preconditions.checkNotNull(right);
+			}
+		};
+
+		new BinaryExpression(symbol, left, right);
+	}
+
 	@Test
 	public void execute_callsInfix(@Injectable final InfixSymbolTest.DummySymbol symbol, @Mocked final Expression expression) {
 		new Expectations() {
@@ -18,6 +31,17 @@ public class BinaryExpressionTest {
 			}
 		};
 
-		(new BinaryExpression(symbol, expression, expression)).execute(Optional.<Map<String, Expression>>absent());
+		new BinaryExpression(symbol, expression, expression).execute(Optional.<Map<String, Expression>>absent());
+	}
+
+	@Test
+	public void execute_checksForNullArguments(@Injectable final InfixSymbolTest.DummySymbol symbol, @Mocked final Expression expression, @Mocked final Optional<Map<String, Expression>> variables) {
+		new Expectations(Preconditions.class) {
+			{
+				Preconditions.checkNotNull(variables);
+			}
+		};
+
+		new BinaryExpression(symbol, expression, expression).execute(variables);
 	}
 }
