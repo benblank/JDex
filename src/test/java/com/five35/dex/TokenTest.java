5db17cbe19b88a40478392b848212eb1ba0743ad
Ben Blank <ben.blank@gmail.com>
2014-09-22 15:13:41 -0700

    null-check ALL THE THINGS (Token)

--- src/test/java/com/five35/dex/TokenTest.java
+++ src/test/java/com/five35/dex/TokenTest.java
@@ -1,12 +1,20 @@
 package com.five35.dex;
 
+import com.google.common.base.Preconditions;
+import mockit.Expectations;
+import mockit.Mocked;
 import org.junit.Test;
 
-@SuppressWarnings({ "javadoc", "static-method" })
+@SuppressWarnings({ "javadoc", "static-method", "unused" })
 public class TokenTest {
-	@SuppressWarnings("unused")
-	@Test(expected = Exception.class)
-	public void ctor_throwsOnNullValue() {
-		new Token(0, null);
+	@Test
+	public void ctor_checksForNullArguments(@Mocked final String string) {
+		new Expectations(Preconditions.class) {
+			{
+				Preconditions.checkNotNull(string);
+			}
+		};
+
+		new Token(0, string);
 	}
 }
