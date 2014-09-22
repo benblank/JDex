c11425bd1e28f40060c2b5d5de5ef36dfe48bd68
Ben Blank <ben.blank@gmail.com>
2014-09-22 15:02:15 -0700

    clean up TokenizerTest (whoops)

--- src/test/java/com/five35/dex/TokenizerTest.java
+++ src/test/java/com/five35/dex/TokenizerTest.java
@@ -22,10 +22,47 @@ public class TokenizerTest {
 		final List<String> actual = Lists.transform(tokens, TokenizerTest.STRINGS_FROM_TOKENS);
 		final List<String> expected = new ArrayList<>(Arrays.asList(strings));
 
-		// Add the terminal token implicitly, so it doesn't have to be listed on each test.
+		// Add the terminal token implicitly, so it doesn't have to be listed on
+		// each test.
 		expected.add("(end)");
 
 		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void tokenize_addsCorrectIndexes() {
+		final List<Token> tokens = Tokenizer.tokenize("abc123");
+
+		Assert.assertEquals(1, tokens.get(0).getIndex());
+		Assert.assertEquals(4, tokens.get(1).getIndex());
+	}
+
+	@Test
+	public void tokenize_emptyStringProducesNoTokens() {
+		TokenizerTest.assertTokens("");
+	}
+
+	@Test
+	public void tokenize_ignoresLeadingWhitespace() {
+		TokenizerTest.assertTokens(" abc", "abc");
+	}
+
+	@Test
+	public void tokenize_ignoresTrailingWhitespace() {
+		TokenizerTest.assertTokens("abc ", "abc");
+	}
+
+	@Test
+	public void tokenize_ignoresWhitespaceBetweenTokens() {
+		TokenizerTest.assertTokens("abc 123", "abc", "123");
+	}
+
+	@Test
+	public void tokenize_indexesCountWhitespace() {
+		final List<Token> tokens = Tokenizer.tokenize("abc 123");
+
+		Assert.assertEquals(1, tokens.get(0).getIndex());
+		Assert.assertEquals(5, tokens.get(1).getIndex());
 	}
 
 	@Test
@@ -49,6 +86,11 @@ public class TokenizerTest {
 	}
 
 	@Test
+	public void tokenize_splitsBetweenSymbols() {
+		TokenizerTest.assertTokens("++", "+", "+");
+	}
+
+	@Test
 	public void tokenize_splitsBetweenSymbolsAndLetters() {
 		TokenizerTest.assertTokens("+abc", "+", "abc");
 	}
@@ -59,53 +101,12 @@ public class TokenizerTest {
 	}
 
 	@Test
-	public void tokenize_splitsBetweenSymbols() {
-		TokenizerTest.assertTokens("++", "+", "+");
-	}
-
-	@Test
 	public void tokenize_underscoresAreLetters() {
 		TokenizerTest.assertTokens("abc_123", "abc_", "123");
 	}
 
 	@Test
-	public void tokenize_ignoresLeadingWhitespace() {
-		TokenizerTest.assertTokens(" abc", "abc");
-	}
-
-	@Test
-	public void tokenize_ignoresTrailingWhitespace() {
-		TokenizerTest.assertTokens("abc ", "abc");
-	}
-
-	@Test
-	public void tokenize_ignoresWhitespaceBetweenTokens() {
-		TokenizerTest.assertTokens("abc 123", "abc", "123");
-	}
-
-	@Test
-	public void tokenize_emptyStringProducesNoTokens() {
-		TokenizerTest.assertTokens("");
-	}
-
-	@Test
 	public void tokenize_whitespaceStringProducesNoTokens() {
 		TokenizerTest.assertTokens(" ");
-	}
-
-	@Test
-	public void tokenize_addsCorrectIndexes() {
-		final List<Token> tokens = Tokenizer.tokenize("abc123");
-
-		Assert.assertEquals(1, tokens.get(0).getIndex());
-		Assert.assertEquals(4, tokens.get(1).getIndex());
-	}
-
-	@Test
-	public void tokenize_indexesCountWhitespace() {
-		final List<Token> tokens = Tokenizer.tokenize("abc 123");
-
-		Assert.assertEquals(1, tokens.get(0).getIndex());
-		Assert.assertEquals(5, tokens.get(1).getIndex());
 	}
 }
