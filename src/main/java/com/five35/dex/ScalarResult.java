dfa6f86e1dbe524146897172603da033c3844d79
Ben Blank <ben.blank@gmail.com>
2014-09-24 12:50:56 -0700

    add utility methods to ScalarResult

--- src/main/java/com/five35/dex/ScalarResult.java
+++ src/main/java/com/five35/dex/ScalarResult.java
@@ -2,6 +2,7 @@ package com.five35.dex;
 
 import com.google.common.collect.ImmutableMultiset;
 import com.google.common.collect.Multiset;
+import javax.annotation.Nonnull;
 
 /**
  * A result which is a single number value.
@@ -21,5 +22,31 @@ public class ScalarResult implements Result {
 	@Override
 	public Multiset<? extends Result> asSet() {
 		return ImmutableMultiset.of(this);
+	}
+
+	/**
+	 * Indicates whether some other object is "equal to" this one.
+	 * 
+	 * This implementation considers result type, so as to be suitable for use
+	 * by e.g. sets and maps. To determine whether two results are *numerically*
+	 * equal, use `a.asScalar() == b.asScalar()`.
+	 * 
+	 * @param other The object with which to compare.
+	 * @return `true` if this object is equal to the other; `false` otherwise.
+	 */
+	@Override
+	public boolean equals(final Object other) {
+		return other instanceof ScalarResult && this.value == ((ScalarResult) other).value;
+	}
+
+	@Override
+	public int hashCode() {
+		return 31 + Float.floatToIntBits(this.value);
+	}
+
+	@Override
+	@Nonnull
+	public String toString() {
+		return Float.toString(this.value);
 	}
 }
